e3e63b88f4af8c573fbf7106aa2181044064837b
veganedge <scottxbrown@yahoo.com>
2022-09-30 17:39:27 -0700

    deleting HOME.js

--- src/index.js
+++ src/index.js
@@ -1,29 +1,18 @@
 // React Imports
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 // React Router Imports
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from "react-router-dom";
 // Style Imports
-import 'bootstrap/dist/css/bootstrap.css';
-import 'react-bootstrap/dist/react-bootstrap.js';
-import './index.css';
+import "bootstrap/dist/css/bootstrap.css";
+import "react-bootstrap/dist/react-bootstrap.js";
+import "./index.css";
 // Component Imports
-import Home from './pages/Home';
-import Main from './pages/Main';
-import Navigation from './components/Navigation';
-import FooterNavigation from './components/FooterNavigation';
+import Main from "./pages/Main";
+import Navigation from "./components/Navigation";
+import FooterNavigation from "./components/FooterNavigation";
 
-
-{/*const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Router>
-      <Home />
-    </Router>
-  </React.StrictMode>
-);*/}
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Navigation />
