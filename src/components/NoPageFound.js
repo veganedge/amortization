d02e30895e30a24d382686bdea97f887986b6893
veganedge <scottxbrown@yahoo.com>
2022-12-18 13:58:01 -0800

    fix for Issue#3 - go back button

--- src/components/NoPageFound.js
+++ src/components/NoPageFound.js
@@ -1,15 +1,26 @@
+//React Router Imports
+import { useNavigate } from "react-router-dom";
 // React Bootstrap Imports
 import { Button, Col } from "react-bootstrap";
-
+// Component Imports
+import Footer from "./Footer";
 
 function NoPageFound() {
+  const navigate = useNavigate();
+
   return (
     <>
       <Col className="text-center mt-5">
-        <h1>Page Not Found</h1>
-        {/* MAKE THIS ONCLICK WORK */}
-        <Button type="button" variant="secondary">GO BACK</Button>
+        <h1 className="">Page Not Found</h1>
+        <Button
+          type="button"
+          variant="secondary"
+          onClick={() => navigate(-1) || navigate("/")}
+        >
+          GO BACK
+        </Button>
       </Col>
+      <Footer addClass="fixed-bottom" />
     </>
   );
 }
